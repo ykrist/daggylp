78a225ed82acec11ac8124fa890dad39ac3ce9f0
Me <Me>
2021-06-07 09:34:05 +1000

    initial, non-compiling and incorrect prototype

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+mod graph;
+mod edges;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ tests/usage.rs
@@ -0,0 +1,11 @@
+use daggylp::Model;
+
+fn main() {
+    let mut m = Model::new();
+
+    let x = m.add_var(0, 1, 4);
+    let y = m.add_var(1, 0, 2);
+
+
+
+}
